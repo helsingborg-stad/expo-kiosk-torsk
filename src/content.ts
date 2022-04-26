95bcaaa20384cbaedfdeb48d28ca61ea0e760d79
Björn Persson <bjorn.persson@knowit.se>
2022-04-26 10:47:05 +0200

    added image example

--- src/content.ts
+++ src/content.ts
@@ -3,6 +3,7 @@ export default {
     sv: {
       title: `1.`,
       html: `
+      <img src="cod.png" width="60%">
       <p>Torsk är en bottenlevande rovfisk som kan bli upp mot 2 meter lång och kan leva i 25 år. Världsrekordet på största torsk som fångats med fiskespö är 47 kg!</p>
       <p>Torskbeståndet längst den Norska kusten kan delas in i två huvudtyper, kusttorsk och Skrei. Kusttorsken spenderar oftast merparten av sitt liv nära kusten på grunt vatten. Skrei däremot är en arktisk fisk som varje vinter ger sig ut på en lång vandring från Barents hav till den norska kusten för att fortplanta sig.</p> 
       <p>Båten som fångat  vår torsk heter Nordhavet och är en kustnära mindre fiskebåt som är verksam långt upp i Nordnorge. Nordhavet använder sig av skonsamma fiskemetoder med ett redskap som heter snurrevad. Fisken dör inte i näten utan tas upp levande. Snurrevad förstör inte havsbotten till skillnad från de stora bottentrålarna som står för det mesta av industrifisket.</p>
